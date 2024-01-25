8cda2e31fc501206cd3972a5111df800283db2f0
James Osburn <Josburn2@uwyo.edu>
2024-01-24 18:53:04 -0700

    Attempt #1

--- code.test.js
+++ code.test.js
@@ -7,10 +7,9 @@ const testSearch =
     jsc.forall("array nat", function(arr) {
         if(arr.length > 0) {
             arr.sort(function(a, b) { return a - b; });
-            return binarySearch(arr, arr[0]) === 0;
+            return binarySearch(arr, arr[0]) === 0 || arr[0] == arr[binarySearch(arr,arr[0])]; //The code after the || is dealing with duplicate values
         } else {
             return binarySearch(arr, "foo") === -1;
         }
     });
-
 jsc.assert(testSearch);
